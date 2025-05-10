eaad6e8460dffd23cfa6023de4840c722c2863b0
IdrissaMaiga <145261070+IdrissaMaiga@users.noreply.github.com>
2025-05-11 00:57:39 +0200

    dfdf
    dffdfd

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Navbar from "@/components/navbar";
-import HeroSection from "@/components/hero-section";
+
 import AboutSection from "@/components/about-section";
 import ProjectsSection from "@/components/projects-section";
 import SkillsSection from "@/components/skills-section";
@@ -7,6 +6,8 @@ import ContactSection from "@/components/contact-section";
 import Footer from "@/components/footer";
 import ChatButton from "@/components/chat-button";
 import MatrixBackground from "@/components/matrix-background";
+import Navbar from "@/components/navbar";
+import HeroSection from "@/components/hero-section";
 
 export default function Home() {
   return (
